47a77e1530c2b44128abb00da3cd647fabc3ac6f
musafir928 <uganoghli@gmail.com>
2022-03-26 11:52:15 +0100

    add singly linkedlist

--- note_practice/src/main/java/SinglyLinkedList/App.java
+++ note_practice/src/main/java/SinglyLinkedList/App.java
@@ -1,14 +1,16 @@
 package SinglyLinkedList;
 
+import java.util.HashSet;
+
 public class App {
     public static void main(String[] args) {
-        LinkedList l = new LinkedList(0);
-        l.append(new Node(1));
-        l.append(new Node(2));
-        l.append(new Node(3));
-        l.append(new Node(4));
-        l.reverse();
-        l.reverseBtureForce();
-        l.print();
+//        LinkedList l = new LinkedList(0);
+//        l.append(new Node(1));
+//        l.append(new Node(2));
+//        l.append(new Node(3));
+//        l.append(new Node(4));
+//        l.reverse();
+//        l.reverseBtureForce();
+//        l.print();
     }
 }
