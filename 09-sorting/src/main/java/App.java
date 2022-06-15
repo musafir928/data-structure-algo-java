703cb5230f3d29dd1bb447d83e4c9d9e7d8ca115
musafir928 <uganoghli@gmail.com>
2022-06-16 00:31:04 +0200

    add merge sort

--- 09-sorting/src/main/java/App.java
+++ 09-sorting/src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
         int[] myArray = {8,3,4,5,6,2,3,5};
-        System.out.println(Arrays.toString(BubbleSort.bubbleSort(myArray)));
+        MergeSort.mergeSort(myArray);
+        System.out.println(Arrays.toString(myArray));
     }
 }
