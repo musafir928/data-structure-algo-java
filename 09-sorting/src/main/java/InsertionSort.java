0909fd005c02e97a32b597589dfc7011a75677a9
musafir928 <uganoghli@gmail.com>
2022-06-16 00:13:23 +0200

    add insertion sort

--- 09-sorting/src/main/java/InsertionSort.java
+++ 09-sorting/src/main/java/InsertionSort.java
@@ -1,15 +1,13 @@
 public class InsertionSort {
     public static int[] insertionSort(int[] array){
         for (int i = 1; i < array.length; i++) {
-            int toBeInserted= array[i];// save current value in a variable called toBeInserted
-            int j=i-1;// predecessor
-            // this is where we do shifting operation
+            int toBeInserted= array[i];
+            int j=i-1;
             while(j>=0 && array[j]>toBeInserted){
-                // shift elements
                 array[j+1]=array[j];
                 j--;
             }
-            array[j+1]=toBeInserted;// put toBeInserted in its place
+            array[j+1]=toBeInserted;
         }
         return array;
     }
