2e3d549e1fe8f648fc4aaaa5eff2692787f3d45a
musafir928 <uganoghli@gmail.com>
2022-06-16 00:01:59 +0200

    add bubble sort

---
+++ 09-sorting/src/main/java/App.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        int[] myArray = {8,3,4,5,6,2,3,5};
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort(myArray)));
+    }
+}
